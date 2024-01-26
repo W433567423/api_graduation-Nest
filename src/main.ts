75725186e21000517cac35b8763a6f130667ba6a
tutu <t433567423@163.com>
2024-01-26 09:09:28 +0800

    init: 初始化nest项目

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SERVER_PORT } from './config';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
-bootstrap();
+
+bootstrap().then();
