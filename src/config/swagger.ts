0961cdebb8897e24ef0bb5b18a7a1538096250bf
tutu <t433567423@163.com>
2024-01-26 16:53:05 +0800

    build: 配置内置的logger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -5,7 +5,6 @@ const swaggerOptions = new DocumentBuilder()
   .setTitle('Graduation example')
   .setDescription('The API description for tutu の graduation')
   .setVersion('1.0')
-  .addTag('users')
   .build();
 
 // swagger链接前缀
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { SERVER_PORT } from './config';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerOptions, swaggerPrefix } from '@/config/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  // const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   // swagger
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup(swaggerPrefix, app, document);
   await app.listen(SERVER_PORT);
+  logger.warn(`后台系统已开启，运行在localhost:${SERVER_PORT}`);
 }
 
 bootstrap().then();
