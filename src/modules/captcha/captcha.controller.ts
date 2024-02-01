ae3187d8c3b3a3cacba6b1cb343dc5a5f59ed3cb
tutu <t433567423@163.com>
2024-02-01 10:58:43 +0800

    feat: 更新dto

--- src/modules/captcha/captcha.controller.ts
+++ src/modules/captcha/captcha.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, Query, Session } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+  Session,
+} from '@nestjs/common';
 import { CaptchaService } from './captcha.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import * as svgCaptcha from 'svg-captcha';
 import { NoAuth } from '@/global/decorator';
-import { getCaptchaDto } from '@/modules/captcha/dto/captcha.req.dto';
+import {
+  getCaptchaReqDto,
+  getPhoneCaptchaReqDto,
+} from '@/modules/captcha/dto/captcha.req.dto';
 import { creatValidaCode } from '@/utils';
 import {
   getCaptchaResDto,
@@ -24,7 +34,7 @@ export class CaptchaController {
   @Get()
   async getCaptcha(
     @Session() session: Record<string, any>,
-    @Query() query: getCaptchaDto,
+    @Query() query: getCaptchaReqDto,
   ) {
     const captcha = svgCaptcha.create({
       size: 4,
@@ -42,8 +52,17 @@ export class CaptchaController {
     type: getPhoneCaptchaResDto,
   })
   @Get('phone')
-  async getPhoneCaptcha(@Session() session: Record<string, any>) {
+  async getPhoneCaptcha(
+    @Query() query: getPhoneCaptchaReqDto,
+    @Session() session: Record<string, any>,
+  ) {
+    const phoneRex = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    const { phoneNum } = query;
+    if (!phoneNum || phoneNum.length !== 11 || phoneRex.test(phoneNum)) {
+      throw new HttpException('手机号不正确', HttpStatus.FORBIDDEN);
+    }
     const code = creatValidaCode();
+    console.log('手机号码', phoneNum);
     session.captcha = code;
     return code;
   }
--- src/modules/captcha/dto/captcha.req.dto.ts
+++ src/modules/captcha/dto/captcha.req.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class getCaptchaDto {
+export class getCaptchaReqDto {
   @ApiProperty({
     description: 'svg的长度(px)',
     example: 200,
     default: 200,
     required: false,
   })
-  readonly width: number;
+  readonly width?: number;
 
   @ApiProperty({
     description: 'svg的宽度(px)',
@@ -15,5 +15,13 @@ export class getCaptchaDto {
     default: 200,
     required: false,
   })
-  readonly height: number;
+  readonly height?: number;
+}
+export class getPhoneCaptchaReqDto {
+  @ApiProperty({
+    description: '手机号码',
+    example: 17700000000,
+    default: 17700000000,
+  })
+  readonly phoneNum: string;
 }
