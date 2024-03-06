5f440eb6c0b466b92b70c16906778245f9c75cd6
tutu <t433567423@163.com>
2024-03-06 11:05:03 +0800

    fix: 更新跨域

--- src/config/cos.config.ts
+++ src/config/cos.config.ts
@@ -11,7 +11,7 @@ export const corsConfig: CorsOptions = {
     'Authorization',
   ],
   //定义了允许跨域访问的来源。可以是一个字符串，也可以是一个字符串数组
-  origin: ['http://localhost:8003', 'http://g.wtututu.top'],
+  origin: ['http://localhost:8003', 'https://g.wtututu.top'],
   // 指定了是否允许跨域请求携带认证信息，如 Cookies、Authorization 等
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
